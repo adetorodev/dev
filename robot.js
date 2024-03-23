adc60270c6aba20913724606416b8a103d8f2dc3
Ezekiel Adetoro <adetoroe787@gmail.com>
2024-03-23 12:06:00 +0100

    task

--- robot.js
+++ robot.js
@@ -29,4 +29,34 @@ function buildGraph(edges) {
 }
 
 const roadGraph = buildGraph(roads);
-console.log(roadGraph)
+// console.log(roadGraph)
+
+
+class VillageState {
+    constructor(place, parcels){
+        this.place = place
+        this.parcels = parcels
+    }
+
+    move(destination){
+        if(!roadGraph[this.place].includes(destination)){
+            return this
+        } else {
+            let parcels = this.parcels.map( p => {
+                if(p.place != this.place) return p;
+                return {place: destination, address: p.address};
+            }).filter(p => p.place != p.address);
+            return new VillageState(destination, parcels)
+        }
+    }
+}
+
+let first = new VillageState(
+    "Post office", [{place: "Post office", address: "Alice's house"}]
+)
+
+let next = first.move("Alice house")
+
+console.log(next.place)
+console.log(next.parcels)
+console.log(first.place)
